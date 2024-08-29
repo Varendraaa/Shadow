bc5dca659235bb9578bf95ec6009c0b145012313
Varendra Saith <136855587+Varendraaa@users.noreply.github.com>
2024-08-29 08:11:44 +0100

    Delete stone_tex.tsx

--- stone_tex.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="stone_tex" tilewidth="248" tileheight="248" tilecount="1" columns="1">
- <image source="stone_tex.jpg" width="483" height="480"/>
-</tileset>
